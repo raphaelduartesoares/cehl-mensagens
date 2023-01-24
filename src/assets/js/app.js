828af865b27d8302c8f20018d463391aa6117a94
Raphael Soares <raphael.duartesoares@gmail.com>
2023-01-23 22:27:16 -0300

    refactor: rename data.json and includes data source

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,5 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
-import data from "/src/assets/data/data.json" assert { type: "json" };
+import data from "/src/assets/data/livro-fonte-viva.json" assert { type: "json" };
 
 createApp({
     data() {
